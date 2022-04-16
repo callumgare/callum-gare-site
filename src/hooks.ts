2df5f9f82b05f20617ea88ed52207e928617b419
Callum Gare <callum@gare.au>
2022-04-16 18:42:18 +1000

    Use SvelteKit hook for setting theme colours

---
+++ src/hooks.ts
@@ -0,0 +1,24 @@
+/** @type {import('@sveltejs/kit').Handle} */
+import { getRandomColourPallet } from './lib/colour-pallet';
+export async function handle({ event, resolve }) {
+	return await resolve(event, {
+		transformPage: ({ html }) => insertColourPalletVars(html)
+	});
+}
+
+function insertColourPalletVars(html: string): string {
+	const colourPallet = getRandomColourPallet();
+
+	return html.replace(
+		'</head>',
+		`<meta name="theme-color" media="(prefers-color-scheme: light)" content="${colourPallet.light}">
+			<meta name="theme-color" media="(prefers-color-scheme: dark)" content="${colourPallet.dark}">
+			<style>
+				:root {
+					--colour-light: ${colourPallet.light};
+					--colour-dark: ${colourPallet.dark};
+				}
+			</style>
+			</head>`
+	);
+}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,28 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import { getRandomColourPallet } from '../lib/colour-pallet';
-
-	const colourPallet = getRandomColourPallet();
-
-	if (typeof document !== 'undefined') {
-		const rootElm = document.querySelector(':root') as HTMLElement;
-		rootElm.style.setProperty('--colour-light', colourPallet.light);
-		rootElm.style.setProperty('--colour-dark', colourPallet.dark);
-	}
-	let themeColour = colourPallet.light;
-	if (typeof window !== 'undefined') {
-		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-			themeColour = colourPallet.dark;
-		}
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-			themeColour = colourPallet[e.matches ? 'dark' : 'light'];
-		});
-	}
 </script>
-
-<svelte:head>
-	<meta name="theme-color" content={`${themeColour}`} />
-</svelte:head>
 
 <main>
 	<slot />
